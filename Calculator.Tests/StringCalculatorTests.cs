43f9580ea4ed6cb5bc6478aac7cc20c7f6e835eb
Kamruz Jaman <jammykam@gmail.com>
2015-07-19 23:23:04 +0100

    Initial Commit

---
+++ Calculator.Tests/StringCalculatorTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Calculator.Tests
+{
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+        [Test]
+        public void Add_EmptyString_ReturnsZero()
+        {
+            IStringCalculator stringCalculator = new StringCalculator();
+            int result = stringCalculator.Add("");
+            Assert.AreEqual(0, result);
+        }
+    }
+}
